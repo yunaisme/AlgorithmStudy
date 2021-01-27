0ad941a4fe02397706bf0d285e7fd9b554186629
yunaisme <57870185+yunaisme@users.noreply.github.com>
2021-01-27 20:39:55 +0900

    Create 210127_Picking_Numbers.c

---
+++ hackerrank/210127_Picking_Numbers.c
@@ -0,0 +1,187 @@
+/* https://www.hackerrank.com/challenges/picking-numbers/problem */
+/* 2021-01-26 */
+
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+max index가 100이니까 크기가 100인 배열 arr을 0으로 우선 초기화 해놓고,
+파라미터로 받은 a 배열의 각 value를 arr의 인덱스로 인지하여 배열에 넣어준다.
+그 arr을 이용하여 가장 긴 배열을 찾으면 될 것
+*/
+
+int arr[100];
+
+int pickingNumbers(int a_count, int* a) {
+    
+    int result=0;
+    
+    for(int i=0; i<a_count; i++) arr[a[i]]++;
+    
+    for(int j=0; j<99; j++){
+        if(result<(arr[j]+arr[j+1]))
+            result = arr[j]+arr[j+1];
+    }
+    
+    return result;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    char** a_temp = split_string(rtrim(readline()));
+
+    int* a = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        int a_item = parse_int(*(a_temp + i));
+
+        *(a + i) = a_item;
+    }
+
+    int result = pickingNumbers(n, a);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
