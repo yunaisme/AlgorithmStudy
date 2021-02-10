de5eedf3ad225cc2817d010c6c38c7ce0384cf82
yunaisme <57870185+yunaisme@users.noreply.github.com>
2021-02-11 04:18:07 +0900

    Create 210211_Circular_Array_Rotation.c

---
+++ hackerrank/210211_Circular_Array_Rotation.c
@@ -0,0 +1,17 @@
+/* https://www.hackerrank.com/challenges/circular-array-rotation/problem */
+/* 2021-02-11 */
+
+#include <stdio.h>
+
+int main() {
+  int n, k, q, qx;
+  scanf("%d %d %d", &n, &k, &q);
+  int nums[n];
+  for (int ni = 0; ni < n; ni++)
+    scanf("%d", &nums[ni]);
+  while (q--) {
+    scanf("%d", &qx);
+    printf("%d\n", nums[(n + qx - (k % n)) % n]);
+  }
+  return 0;
+}
