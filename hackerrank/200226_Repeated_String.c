9f4675becee016996f4a802204fa09ad40bd74c5
yunaisme <57870185+yunaisme@users.noreply.github.com>
2021-02-26 22:05:48 +0900

    Create 200226_Repeated_String.c

---
+++ hackerrank/200226_Repeated_String.c
@@ -0,0 +1,29 @@
+/*  */
+/* 2021-02-26 */
+
+#include <stdio.h>
+#include<string.h>
+#define MAX 100
+
+int main(void) {
+    char str[MAX];
+    long n, count=0;
+    int i;
+    
+    gets(str);
+    scanf("%ld", &n);
+    
+    for(i=0;i<strlen(str);i++)
+        if(str[i]=='a')
+            count++;
+            
+    count*=(n/strlen(str));
+    
+    for(i=0;i<n%strlen(str);i++)
+        if(str[i]=='a')
+            count++;
+            
+    printf("%ld", count);
+    
+    return 0;
+}
