c214ef15c59fd3a88c76fd6fa04cc287f54dedd0
yunaisme <57870185+yunaisme@users.noreply.github.com>
2021-04-11 02:38:34 +0900

    Create 210411_Super_Reduced_String.c

---
+++ hackerrank/210411_Super_Reduced_String.c
@@ -0,0 +1,43 @@
+/* https://www.hackerrank.com/challenges/reduced-string/problem */
+/* 2021-04-11 */
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    
+    char* s = (char *)malloc(99999 * sizeof(char));
+    scanf("%s", s);
+    int l, i=0, flag;
+    l=strlen(s);
+    
+    do {
+        flag=0;
+        
+        for(i=0;i<l-1;) {
+            if(s[i]==s[i+1]) {
+                flag=1;
+                for(int j=i;j<l-2;j++)
+                    s[j]=s[j+2];
+                l=l-2;
+            }
+            
+            else
+                i++;
+        }
+        
+        s[l]='\0';
+        
+    } while(flag==1&&l>0);
+
+    if(l!=0)
+        printf("%s",s);
+    else  
+        printf("Empty String");
+        return 0;
+}
